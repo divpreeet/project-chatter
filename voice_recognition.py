61eb55229d46454afd0acfca9164d2ce57fc2ee0
divpreeet <ytbraced@gmail.com>
2025-06-08 17:55:12 +0530

    sounddevice addd

--- voice_recognition.py
+++ voice_recognition.py
@@ -1,22 +1,26 @@
 import speech_recognition as sr
+import sounddevice as sd
+import numpy as np
 
 def recognize_speech():
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source, duration=1)
-        audio = recognizer.listen(source)
-        usr_message = ""
+    duration = 5 
+    sample_rate = 16000
 
-# transcribe audio
+    print("Listening...")
+    audio_array = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
+    sd.wait()
+    audio_array = audio_array.flatten()
+
+    audio_bytes = audio_array.tobytes()
+    audio = sr.AudioData(audio_bytes, sample_rate, 2)  
+
+    usr_message = ""
     try:
         usr_message = recognizer.recognize_google(audio)
         print(f"this is what you said - {usr_message}")
-    
     except Exception as e:
         print(e)
 
-
     return usr_message
-
-
